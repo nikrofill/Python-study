fbd7522aeee26d29bab775aa353c7210dfaa98cb
nikrofill <rk@easypos.tech>
2021-10-18 09:28:08 +0300

    UPD

--- Lesson 7/Lesson 7. HW 9.py
+++ Lesson 7/Lesson 7. HW 9.py
@@ -7,14 +7,11 @@ print('Задача 9. ...Теперь можно посчитать и свою
 # Пользователь вводит 10 чисел.
 # Напишите программу, которая проверяет, упорядочены ли они по возрастанию.
 c = 0
-salary = 0
-c1 = 0
-for n in range(10):
+salary = int(input("Введите число: "))
+for n in range(9):
+    c = salary
     salary = int(input("Введите число: "))
-    c += salary
-    c1 = c - salary
-    print(c1,c)
-    if(c1 > c):
+    if(c > salary):
         print("Бух, тебя наё :)")  
         break
     
